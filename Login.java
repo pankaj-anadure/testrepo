fa7f973cc9dd66b876e3c52bfa693a3eb94c027b
pankaj-anadure <pankaj-anadure@wissen.com>
2022-04-11 17:44:22 +0530

    Changed Login

--- Login.java
+++ Login.java
@@ -1,3 +1,11 @@
 public class Login
 {
+	public void username()
+	{
+		System.out.println("Username should be min 5 characters long");
+	}
+	public void password()
+	{
+		System.out.println("Password should be min of 6 letters");
+	}
 }
